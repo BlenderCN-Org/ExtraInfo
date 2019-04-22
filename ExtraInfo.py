ffa20a6879438eebab91ad9adae50e8d762f580a
daniel.grauer <grauer.d@gmail.com>
2019-04-22 18:22:45 +0200

    bumped version; dynamic offset; updated to new tool header; added information about selected objects; unregister added; lot of pep8 formating

--- ExtraInfo.py
+++ ExtraInfo.py
@@ -1,4 +1,3 @@
-import bgl
 import blf
 import bpy
 
@@ -29,7 +28,7 @@ bl_info = {
     "name": "ExtraInfo",
     "description": "Show Extra Information in Viewport",
     "author": "zebus3d",
-    "version": (0, 0, 2),
+    "version": (0, 0, 4),
     "blender": (2, 80, 0),
     "location": "View3D",
     "wiki_url": "",
@@ -43,35 +42,27 @@ font_info = {
 }
 
 
-
 def draw_callback_px(self, context):
     # esto se calcula cada pasada de dibujado del viewport:
 
     display = []
-
     font_id = font_info["font_id"]
     
     uiScale = bpy.context.preferences.view.ui_scale
+    # print("uiscale: ", uiScale)
+    x_offset = 24 * uiScale + (bpy.context.area.regions[4].x - bpy.context.area.regions[1].x)   # regions (HUD-HEADER)
+    # for region in bpy.context.area.regions:
+    #     print('region type: ', region.type)
+    #     print('region x/y: ', region.x, ' / ', region.y)
+    #     print('region width/height: ', region.width, ' / ', region.height)
+    # print(40*"=")
 
-    x = 19 * uiScale
+    y_offset = (bpy.context.area.regions[3].height + bpy.context.area.regions[4].y) - 110 * uiScale     # regions(UI+HUD)
+    # print("UI height: ", bpy.context.area.regions[2].height)
+    # print("hud y pos: ", bpy.context.area.regions[3].y)
+    # print("hud offset: ", bpy.context.area.regions[2].height + bpy.context.area.regions[3].y)
 
-    toolbarWidth = bpy.context.area.regions[1].width 
-
-    # offsetTextInTop = 86 * uiScale
-    
-
-    if uiScale >= 1:
-        offsetTextInTop = 86 * uiScale
-        fontSize = 11 * round(uiScale)
-    else:
-        offsetTextInTop = 86 * uiScale +10
-        fontSize = 11 - 4
-
-
-    yOffset = bpy.context.area.height - offsetTextInTop 
-
-    # fontSize = 11 
-
+    fontSize = int(12 * uiScale)
     blf.size(font_id, fontSize, 72)
     
     # sombra:
@@ -87,9 +78,9 @@ def draw_callback_px(self, context):
     blf.shadow_offset(font_id, 1, -1)
     
     engines = {
-        'BLENDER_EEVEE' : 'Eevee',
-        'BLENDER_WORKBENCH' : 'Workbench',
-        'CYCLES' : 'Cycles'
+        'BLENDER_EEVEE': 'Eevee',
+        'BLENDER_WORKBENCH': 'Workbench',
+        'CYCLES': 'Cycles'
     }
 
     re = 'Engine: ' + engines.get(bpy.context.scene.render.engine)
@@ -97,9 +88,13 @@ def draw_callback_px(self, context):
 
     view_layer = bpy.context.view_layer
     stats = bpy.context.scene.statistics(view_layer).split("|")
+    verts, polys, tris = selected_object_info(stats)
 
     if bpy.context.mode == 'OBJECT':
-        ss = stats[2:5]
+        ss = stats[5:5]
+        ss.insert(2, verts)
+        ss.insert(3, polys)
+        ss.insert(4, tris)
         ss.append(stats[-2])
         stats = ss
     elif bpy.context.mode == 'EDIT_MESH':
@@ -129,13 +124,38 @@ def draw_callback_px(self, context):
         if bpy.context.space_data.overlay.show_text:
             for counter, value in enumerate(display):
                 # print(value)
-                value = value.replace(" ","")
-                value = value.replace(":",": ")
+                value = value.replace(" ", "")
+                value = value.replace(":", ": ")
+                value = value.replace("/", " / ")
                 # print(value)
                 increment = (20*counter*uiScale)
-                blf.position(font_id, x+toolbarWidth, yOffset-increment-rendered*uiScale, 0)
+                blf.position(font_id, x_offset, y_offset-increment-rendered*uiScale, 0)
                 blf.draw(font_id, value)
 
+
+def selected_object_info(stats):
+    verts = 0
+    polys = 0
+    tris = 0
+
+    verts_scene = stats[2].split(':')[1]
+    polys_scene = stats[3].split(':')[1]
+    tris_scene = stats[4].split(':')[1]
+
+    for obj in bpy.context.selected_objects:
+        verts += len(obj.data.vertices)
+        polys += len(obj.data.polygons)
+
+        for face in obj.data.polygons:
+            vertices = face.vertices
+            triangles = len(vertices) - 2
+            tris += triangles
+
+    verts = '{}{}{}{}'.format('Verts:', polys, "/", verts_scene)
+    polys = '{}{}{}{}'.format('Faces:', polys, "/", polys_scene)
+    tris = '{}{}{}{}'.format('Tris:', tris, "/", tris_scene)
+    # print("selected mesh info: ", verts, polys, tris)
+    return verts, polys, tris
 
 
 def init():
@@ -147,8 +167,10 @@ def init():
 def register():
     init()
 
+
 def unregister():
-    pass
+    bpy.types.SpaceView3D.draw_handler_remove(font_info["handler"], 'WINDOW')
+
 
 if __name__ == "__main__":
     register()
